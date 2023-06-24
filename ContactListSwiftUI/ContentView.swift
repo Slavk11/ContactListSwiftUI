a1065cc4f52bb447174105f03a00dfa605807ae4
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-24 21:32:47 +0300

    refactoring

--- ContactListSwiftUI/ContentView.swift
+++ ContactListSwiftUI/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            TabView {
+                ContactsView()
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Contacts")
+                    }
+                ListView()
+                    .tabItem {
+                        Image(systemName: "phone")
+                        Text("Numbers")
+                    }
+            }
+            .navigationBarTitle("Contact List")
         }
-        .padding()
     }
 }
 
